07608d31154d1603eb029cc1e35ee0912cffb41e
Vinay Relangi <relangi.vinay45@gmail.com>
2025-09-18 21:19:12 +0530

    Fix static images folder case issue

--- Bank/settings.py
+++ Bank/settings.py
@@ -87,7 +87,7 @@ TEMPLATES = [
 
 # Static files
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [BASE_DIR / "static"] 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
